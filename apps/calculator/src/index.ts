cb2f723271590f0b50e346c81182294f4fe730b3
ajisyahroni <aji.0508@students.amikom.ac.id>
2024-07-31 16:21:54 +0700

    init

---
+++ apps/calculator/src/index.ts
@@ -0,0 +1,5 @@
+import { add } from 'addition';
+import { subtract } from 'subtraction';
+
+console.log("Addition of 5 and 3:", add(5, 3));
+console.log("Subtraction of 5 from 3:", subtract(5, 3));
---
+++ apps/circle-area/src/index.ts
@@ -0,0 +1,5 @@
+import { multiply } from 'multiply';
+const getAreaOfCircle = (r: number) => {
+    return Math.PI * (r ** 2)
+}
+console.log('circle area with r : 7 = ', getAreaOfCircle(7));
